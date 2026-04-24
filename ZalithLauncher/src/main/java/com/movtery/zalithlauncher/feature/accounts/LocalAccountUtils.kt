bc05f7c9947bafdc5781e28feea92a1525ff4c99
MatrixhKal <lucascomplicado1233@gmail.com>
2026-04-24 07:21:59 -0300

    Simplify account usage check in LocalAccountUtils

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/feature/accounts/LocalAccountUtils.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/feature/accounts/LocalAccountUtils.kt
@@ -12,11 +12,7 @@ class LocalAccountUtils {
     companion object {
         @JvmStatic
         fun checkUsageAllowed(listener: CheckResultListener) {
-            if (AccountsManager.hasMicrosoftAccount()) {
-                listener.onUsageAllowed()
-            } else {
-                listener.onUsageDenied()
-            }
+            listener.onUsageAllowed()
         }
 
         @JvmStatic
